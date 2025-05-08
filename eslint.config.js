667d93261e64114de824feb6aaead58ce161ac7d
zenji <canhtoannguyen60@gmail.com>
2025-05-08 11:02:28 +0700

    feat: setup eslint

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["error", {
+        "argsIgnorePattern": "^_*",
+        "varsIgnorePattern": "^_*",
+        "caughtErrorsIgnorePattern": "^_*$"
+      }],
+      "@typescript-eslint/ban-ts-comment": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off"
+    }
+  }
+];
